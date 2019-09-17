477e8227c65822f929a1e806aa6edd6d38fdfd25
Dmytro <voronok16@gmail.com>
2019-09-17 16:36:46 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.430516ddd8658a7093e02ac72f479b0c.js"
 );
 
 self.addEventListener('message', (event) => {
